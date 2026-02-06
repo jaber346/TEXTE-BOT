7d0136f7303961580b2e39573fae34248c024789
jaber346 <jaberporgo@gmail.com>
2026-02-06 17:22:31 +0000

    Add files via upload

---
+++ librairie/docs.js
@@ -0,0 +1,34 @@
+export const contextInfo = {
+  forwardingScore: 999,
+  isForwarded: true,
+  forwardedNewsletterMessageInfo: {
+    newsletterJid: '120363330645505280@newsletter',
+    newsletterName: "TECH DEV MESSY",
+    serverMessageId: 150
+  }
+};
+
+export const image = { 
+ externalAdReply: {
+    title: "",
+    body: "",
+    thumbnailUrl: "",
+    mediaType: 1,
+    renderLargerThumbnail: true,
+    sourceUrl: "https://whatsapp.com/channel/0029VbBYMyZIyPtOEnuT0S04"
+  }
+};.
+
+export const fakevCard = {
+ key: {
+    fromMe: false,
+    participant: "0@s.whatsapp.net",
+    remoteJid: "status@broadcast"
+    },
+    message: {
+    contactMessage: {
+    displayName: "© baseBot Toji Zenin",
+    vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Meta\nORG:META AI;\nTEL;type=CELL;type=VOICE;waid=254101022551:+254101022551\nEND:VCARD`
+    }
+  }
+};
